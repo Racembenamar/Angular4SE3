b027306078ac09211fc03d1d7aa99a41985dcc77
Racembenamar <racem.benamar@esprit.tn>
2023-02-23 23:11:08 +0100

    atelier3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,33 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import {FormsModule} from "@angular/forms";
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { OffreEmploiComponent } from './offre-emploi/offre-emploi.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import {RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import {AddProductComponent} from "./add-product/add-product.component";
+import { TemplateDrivenFormComponentComponent } from './template-driven-form-component/template-driven-form-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    NavBarComponent,
+    NotFoundComponent,
+    OffreEmploiComponent,
+    ProductDetailsComponent,
+    AddProductComponent,
+    TemplateDrivenFormComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterOutlet,
   ],
   providers: [],
   bootstrap: [AppComponent]
